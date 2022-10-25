9c94756e9cd499af3e89b59b0d15dad3086e0a47
jessicashong <jessicashong12@gmail.com>
2022-10-25 11:56:16 -0500

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,20 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log('you clicked the button!');//to ensure that what you want to happen is happening
+  
+  
+  
+  
+  
+  return 'generated password will return here';//<---whatever gets returned based on prompts
+}
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //make generate password function; values should be entered
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
